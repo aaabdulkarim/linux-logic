49becaf8471fc828cfc7eddb233f60e0415f7329
amadeus <aabdulkarim@student.tgm.ac.at>
2024-12-19 17:32:12 +0100

    Models geschrieben und login API Methode ansatz

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from dotenv import load_dotenv, get_key
-from sqlmodel import create_engine, select
+from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+
+# docs: https://fastapi.tiangolo.com/tutorial/sql-databases/
 
 # Laden des Connection Strings
 load_dotenv()
@@ -10,14 +14,57 @@ connectionString = get_key(".env", "CONNECTION_STRING")
 
 
 # Das Erstellen der psql/Neon Engine
-connect_args = {"check_same_thread": False}
-engine = create_engine(connectionString, connect_args=connect_args)
+engine = create_engine(connectionString)
 
+
+class User(SQLModel, table=True):
+    """
+    Model Klasse für einen Linux Logic user
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    username: str = Field(index=True)
+    email: str | None = Field(default=None, index=True)
+    password_hash: int
+
+
+
+# FastAPI App Variable
 app = FastAPI()
 
 
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+@app.get("/login/{userId}")
+async def login(userId : int, session: SessionDep):
+    user = session.get(User, userId)
+
+    if not user:
+        raise HTTPException(status_code=404, detail=f"User with Id {userId} not found")
+    return user        
+
+
+@app.get("/login/")
+async def login(userName : str, userPassword : str, session: SessionDep):
+    """
+    Die Datenbank wird nach userNamen durchsucht und wenn das Passwort übereinstimmt, dann wird true zurückgegeben
+    """
+    user = session.exec(select(User))
+
+
+@app.post("/register")
+async def register(userModel : User, session: SessionDep):
+    """
+    Ein User wird registriert und zur Datenbank hinzugefügt
+    """
+
+
 @app.get("/progress")
-async def getProgress(userId):
+async def getProgress(userId : int, session: SessionDep):
     """
     Der Progress wird als Zahl zurückgegeben. Die Zahl ist die ID des Progress.
     """
