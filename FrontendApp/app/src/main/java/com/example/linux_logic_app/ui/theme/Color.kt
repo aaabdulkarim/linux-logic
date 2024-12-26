38853749bea2ecbe76d069813d48e33954f63e90
Batscho <bacho08lasha08@gmail.com>
2024-12-26 15:19:24 +0100

    DA Linux Logic - Frontend App [Commit 1]: Es wird nun bereits im vorhinein an der Visualität (Color, Theme, ...) gearbeitet, um diese später direkt zu verwenden.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/ui/theme/Color.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/ui/theme/Color.kt
@@ -9,3 +9,18 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// Global
+val LiloMain = Color(0xFF569191)
+val LiloOrange = Color(0xFFFF8c00)
+
+// Light Mode
+val LiloLight = Color(0xFFb5e7de)
+val LiloLightSec = Color(0xFFA0CDC5)
+val LiloLightText = Color(0xFF404242)
+
+// Dark Mode
+val LiloDark = Color(0xFF404242)
+val LiloDarkSec = Color(0xFF4E5050)
+val LiloDarkText = Color(0XFFFFFFFF)
+
--- FrontendApp/app/src/main/java/com/example/linux_logic_app/ui/theme/Theme.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.linux_logic_app.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -12,17 +11,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
+    /* primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
+    */
+
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    /* primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
-    /* Other default colors to override
+    Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
