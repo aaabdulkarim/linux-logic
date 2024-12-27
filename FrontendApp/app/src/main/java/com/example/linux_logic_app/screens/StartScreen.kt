b3c3c2a24916cef07f0a7eac5a737171183af4c5
Batscho <bacho08lasha08@gmail.com>
2024-12-27 21:25:36 +0100

    DA Linux Logic - Frontend App [Commit 13]: Morgen wird intensiver damit fortgesetzt.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/StartScreen.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/StartScreen.kt
@@ -3,28 +3,16 @@ package com.example.linux_logic_app.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.motionEventSpy
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.example.linux_logic_app.R
-import com.example.linux_logic_app.ui.theme.*
 
 @Composable
 fun StartScreen() {
@@ -39,6 +27,11 @@ fun StartScreen() {
             modifier = Modifier
                 .fillMaxSize()
         )
+    }
+    Row(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+
     }
 
 }
