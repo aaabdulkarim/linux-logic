6466a1c732d2ef2636ecdeedea88584e0d9b69fa
Batscho <bacho08lasha08@gmail.com>
2024-12-28 15:44:14 +0100

    DA Linux Logic - Frontend App [Commit 15]: Gerade versuche ich die Linux Logic Einsteiger Seite schön zu designen und zu konstruieren. Permission für ein Redirecting wurden im Manifest gesetzt.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/StartScreen.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/StartScreen.kt
@@ -3,10 +3,14 @@ package com.example.linux_logic_app.screens
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,8 +18,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.linux_logic_app.R
 
 @Composable
@@ -30,21 +34,58 @@ fun StartScreen() {
             painter = painterResource(id = R.drawable.linux_logic_start),
             contentDescription = "Abstraktes Aqua Hintergrundbild",
             contentScale = ContentScale.Fit,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-        )
-    }
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Button(
-            onClick = {
-                Log.i("$LocalContext Action", "User performing \"Login\"")
-            }
+                .align(Alignment.BottomCenter)
+                ,
         ) {
-            Text(text = "Anmelden")
+            Text(
+                text = "Willkommen bei LINUX LOGIC\nEine Lernplattform zum Erwerb grundlegender und essenzieller Linux-Kenntnisse"
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(
+                    onClick = {
+                        Log.i("StartScreen", "User is performing - Action: \"Login\" -")
+                    }
+                ) {
+                    Text(
+                        text = "Anmelden"
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        Log.i("StartScreen", "User is performing - Action \"Register\" -")
+                    }
+                ) {
+                    Text(
+                        text = "Registrieren"
+                    )
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.link_logo),
+                    contentDescription = "Ein Link Logo für die Verlinkung zu unserer Webseite",
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier
+                        .clickable {
+                            Log.i("StartScreen", "User clicked link - Action \"Linux Logic Website\" -")
+                        }
+                )
+            }
+
         }
     }
-
 }
