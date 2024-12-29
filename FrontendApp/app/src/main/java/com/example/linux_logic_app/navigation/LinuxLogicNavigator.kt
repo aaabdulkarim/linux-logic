1477558be73b0495867a28269bc3c606f506b991
Batscho <bacho08lasha08@gmail.com>
2024-12-29 15:15:23 +0100

    DA Linux Logic - Frontend App [Commit 22]: Ziel für heute ist die Basisfunktion der Navigation in der Linux Logic App wodruch diese nur mehr erweitert werden muss, und auch das fertigstellen des Logins, sowie die Registrierung.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/navigation/LinuxLogicNavigator.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/navigation/LinuxLogicNavigator.kt
@@ -1,8 +1,11 @@
 package com.example.linux_logic_app.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LinuxLogicNavigator() {
-    // NavHost NavController composables...
+    // NavHost, NavController, composables, route, ...
+    val navController = rememberNavController()
+
 }
