b8b9da137ef422bce98b5b0a2394fa2a8f28d93d
Benjamin Eder <beder@student.tgm.ac.at>
2025-01-14 08:47:04 +0100

    header überarbeitet

--- FrontendWeb/linux-logic/src/components/Header.vue
+++ FrontendWeb/linux-logic/src/components/Header.vue
@@ -1,43 +1,4 @@
-<template>
-    <div class="all grid">
-        <i class="pi pi-bars bars"></i>
-        <div class="icon-container">
-            <i class="pi pi-sun icon"></i>
-            <i class="pi pi-cog icon"></i>
-            <i class="pi pi-user icon"></i>
-        </div>
-    </div>
-</template>
-
-<style>
-    .all {
-        color: white;
-        background-color: #569191;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding-top: 1rem;
-        padding-left: 1rem;
-        padding-right: 1rem;
-
-    }
-
-    .bars {
-        font-size: 2rem;
-        color: white;
-    }
-
-    .icon-container {
-        display: flex;
-        gap: 1.7rem;
-    }
-
-    .icon {
-        font-size: 2rem;
-        color: white;
-    }
-</style>
-
+ 
 <!-- Logo einblendung nach scrollen geht noch nicht-->
 <template>
     <div class="header grid">
@@ -97,7 +58,7 @@
   }
   
   .icon {
-    font-size: 2rem;
+    font-size: 1.6rem;
     color: white;
     transition: transform 0.2s ease;
   }
