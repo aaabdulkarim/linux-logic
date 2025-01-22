ba0104bfed3575dae427de4ca8fc2f7bf562824c
Abdulkarim <aabdulkarim@student.tgm.ac.at>
2025-01-23 00:33:25 +0100

    Test Code aus der Test Branch

---
+++ Backend/test/test.js
@@ -0,0 +1,44 @@
+// Test WebSocket
+const socket = new WebSocket("ws://localhost:8000/ws");
+
+// Elements
+var input = document.getElementById("inputTerminal");
+var button = document.getElementById("sendButton");
+
+
+function appendUlElement(outputString, className) {
+  var ulEl = document.getElementById("ausgaben");
+  var liOutputEl = document.createElement("article")
+  liOutputEl.className = className
+  liOutputEl.innerHTML = outputString
+  ulEl.appendChild(liOutputEl)
+
+}
+
+
+
+// Listen for messages
+socket.addEventListener("message", (event) => {
+  console.log("Output", event.data);
+  appendUlElement(event.data, "left");
+});
+
+
+
+function handleInputAction(input){
+  socket.send(input.value);
+  appendUlElement(input.value, "right");
+  input.value = ''
+
+}
+
+
+// HTML JS Functionality
+button.addEventListener("click", (event) => handleInputAction(input));
+input.addEventListener(
+    "keydown", function (e) {
+    if (e.key === 'Enter') {
+      handleInputAction(input)
+    }
+});
+
