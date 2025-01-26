5eda2ca54036b8348b36455e194d5bc375b9ed78
Batscho <bacho08lasha08@gmail.com>
2025-01-26 19:22:10 +0100

    DA Linux Logic - Frontend App [Commit 66]: Es wird das nächste mal weiter gearbeitet an der Customization vom Terminal. Was habe ich heute geschafft: Es wurde die gesamte App bezüglich Icons verändert, da die Material Icons Extended Library implementiert wurde. Die App besitzt nun sowohl Lightmode als auch Darkmode. Und der MainScreen wurde etwas verändert, sowie der Login und die Register-Seite. Stand 26.01.2025 19:22

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/CustomizeScreen.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/CustomizeScreen.kt
@@ -1,20 +1,64 @@
 package com.example.linux_logic_app.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Palette
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.linux_logic_app.ui.theme.LiloMain
 
 @Composable
 fun CustomizeScreen() {
-    Box(
+    Column(
         modifier = Modifier
+            .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
             .padding(16.dp)
+            .verticalScroll(rememberScrollState())
+
     ) {
+        Text(
+            text = "Passe dein Terminal individuell an!",
+            style = MaterialTheme.typography.bodyLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.TwoTone.Palette,
+                contentDescription = "Palette Icon for Customization",
+                tint = LiloMain
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Text(
+                text = "Farboptionen:",
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
 
     }
 }
