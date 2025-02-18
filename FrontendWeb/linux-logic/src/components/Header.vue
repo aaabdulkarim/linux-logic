958ce49f31f327c51cb023875899f2cddb4c4e30
Benjamin Eder <beder@student.tgm.ac.at>
2025-02-18 10:54:03 +0100

    header

--- FrontendWeb/linux-logic/src/components/Header.vue
+++ FrontendWeb/linux-logic/src/components/Header.vue
@@ -1,53 +1,4 @@
 <template>
-<<<<<<< HEAD
-    <div class="header grid">
-    <img src="@/assets/LogoLinuxLogic.webp" alt="Logo" v-show="showLogo"  class="logo-sticky" />
-      <div class="icon-container">
-        <i class="pi pi-sun icon"></i>
-        <i class="pi pi-cog icon"></i>
-        <i class="pi pi-user icon" @click="navigateToProfile"></i>
-
-        <i class="pi pi-bars icon"></i>
-      </div>
-
-    </div>
-  </template>
-  <script>
-  export default {
-  methods: {
-    navigateToProfile() {
-      this.$router.push({ name: 'profil' });
-    }
-  }
-};
-  showLogo = false;
-  </script>
-  <style scoped>
-  .header {
-    position: sticky;
-    top: 0;
-    background-color: #569191;
-    padding: 1rem 1rem;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    transition: background-color 0.3s ease;
-    z-index: 10;
-  }
-  .icon-container {
-    display: flex;
-    gap: 1.7rem;
-  }
-  .icon {
-    font-size: 1.6rem;
-    color: white;
-    transition: transform 0.2s ease;
-  }
-  .icon:hover {
-    transform: scale(1.05);
-  }
-  </style>
-=======
   <div class="header grid">
     <a href="/#/about" class="logo-link">
       <img src="@/assets/LogoLinuxLogic.webp" alt="Logo" class="logo-sticky" />
@@ -97,4 +48,3 @@
   transform: scale(1.05);
 }
 </style>
->>>>>>> 9d75b2622f3f07a119c4f1dba34e6c82bb48c571
