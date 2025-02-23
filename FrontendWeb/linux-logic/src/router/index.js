ca7680ea6761c6e0cca2348fbccc5e48f38fe096
Abdulkarim <aabdulkarim@student.tgm.ac.at>
2025-02-23 02:51:56 +0100

    New Routing

--- FrontendWeb/linux-logic/src/router/index.js
+++ FrontendWeb/linux-logic/src/router/index.js
@@ -2,13 +2,16 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  
   {
-    path: '/',
-    name: 'home',
+    path: '/about',
+    name: 'about',
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/',
+    redirect: "/about"
   },
   {
     path : "/auswahl",
