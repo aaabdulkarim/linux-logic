da9758e99db1f2bb109246a6309e42210f635d54
bgogoladze <bgogoladze@student.tgm.ac.at>
2025-03-04 10:50:41 +0100

    DA Linux Logic - Frontend App [Commit 126]: Es wurden die Logik für die Umsetzung des Sublevel bedacht und diskutiert. Zudem wurden die Courses und Subcourses umbenannt in Scenario und Sublevel. Nun geht es an die Logik und dem Schreiben des Codes.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/components/Scenario.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/components/Scenario.kt
@@ -10,10 +10,11 @@ import com.example.linux_logic_app.R
  * @property description Eine kurze Beschreibung des Kurses.
  * @property imageRes Die ID der Bildressource, die mit diesem Kurs verknüpft ist.
  */
-data class Course(
+data class Scenario(
     val name: String,
     val description: String,
-    val imageRes: Int
+    val imageRes: Int,
+    val subcourses: LinkedHashMap<Int, Sublevel>
 )
 
 /**
@@ -22,39 +23,79 @@ data class Course(
  * Namen, Beschreibungen und zugehörigen Bildressourcen.
  */
 val courseList = listOf(
-    Course(
+    Scenario(
         name = "Linux Grundlagen",
         description = "Deine ersten Schritte: Lerne die Grundlagen des Linux-Betriebssystems kennen. Du wirst verstehen, wie du dich im Terminal zurechtfindest, Dateien verwaltest und einfache Aufgaben automatisierst.",
-        imageRes = R.drawable.linux_basics_course
+        imageRes = R.drawable.linux_basics_course,
+        subcourses = linkedMapOf(
+            1 to Sublevel(
+                name = "",
+                description = "Lerne die Geschichte und die grundlegenden Konzepte von Linux kennen. Erfahre, warum Linux in Servern, Embedded Systems und der Softwareentwicklung so weit verbreitet ist."
+            ),
+            2 to Sublevel(
+                name = "",
+                description = "Mache dich mit der Kommandozeile vertraut und lerne grundlegende Befehle wie `ls`, `cd`, `pwd` und `man`, um dich im Dateisystem zu bewegen."
+            ),
+            3 to Sublevel(
+                name = "",
+                description = "Lerne, wie du Dateien erstellst, bearbeitest, verschiebst, umbenennst und löschst. Erfahre, wie du mit Verzeichnissen arbeitest und deren Berechtigungen verwaltest."
+            ),
+            4 to Sublevel(
+                name = "",
+                description = "Verstehe, wie Linux-Prozesse arbeiten, wie du sie mit `ps`, `top` oder `kill` steuerst und wie Benutzer- und Gruppenrechte funktionieren."
+            ),
+            5 to Sublevel(
+                name = "",
+                description = "Lerne, was Shell-Skripte sind und wie du sie erstellst. Erfahre, wie du Skripte ausführbar machst und welche grundlegenden Strukturen es gibt."
+            ),
+            6 to Sublevel(
+                name = "",
+                description = "Nutze Variablen, um Werte zu speichern, und verwende Schleifen (`for`, `while`, `until`), um Abläufe effizient zu automatisieren."
+            ),
+            7 to Sublevel(
+                name = "",
+                description = "Lerne Techniken zur Fehlerbehandlung und Protokollierung, damit deine Skripte robust und fehlertolerant sind."
+            ),
+            8 to Sublevel(
+                name = "",
+                description = "Erfahre, wie du Hintergrundprozesse startest, beendest und steuerst. Nutze `nohup`, `jobs`, `fg`, `bg` und `disown`, um Skripte effizient auszuführen."
+            ),
+        )
     ),
-    Course(
+    Scenario(
         name = "Fortgeschrittene Bash",
         description = "Werde zum Bash-Profi: Erstelleeeeeeeeeeeeeeeeeeeeeee leistungsstarke Bash-Skripte, um deine Arbeit am Computer zu automatisieren und zu beschleunigen. Erweitere deine Kenntnisse in der Shell und werde zum Experten für die Kommandozeile.\nWerde zum Bash-Profi: Erstelle leistungsstarke Bash-Skripte, um deine Arbeit am Computer zu automatisieren und zu beschleunigen. Erweitere deine Kenntnisse in der Shell und werde zum Experten für die Kommandozeile.\nWerde zum Bash-Profi: Erstelle leistungsstarke Bash-Skripte, um deine Arbeit am Computer zu automatisieren und zu beschleunigen. Erweitere deine Kenntnisse in der Shell und werde zum Experten für die Kommandozeile.\nWerde zum Bash-Profi: Erstelle leistungsstarke Bash-Skripte, um deine Arbeit am Computer zu automatisieren und zu beschleunigen. Erweitere deine Kenntnisse in der Shell und werde zum Experten für die Kommandozeile.",
-        imageRes = R.drawable.bash_course
+        imageRes = R.drawable.bash_course,
+        subcourses = linkedMapOf()
     ),
-    Course(
+    Scenario(
         name = "Docker Kenntnisse",
         description = "Beherrsche Docker: Entwickle und verwalte moderne Anwendungen mit Docker Containern. Lerne, wie du deine Anwendungen portabel und skalierbar machst.",
-        imageRes = R.drawable.docker_course
+        imageRes = R.drawable.docker_course,
+        subcourses = linkedMapOf()
     ),
-    Course(
+    Scenario(
         name = "Linux Dateisystem und Navigation",
         description = "Navigiere durch das Linux-Dateisystem: Versteh den Aufbau des Linux-Dateisystems und wie du effizient darin navigierst. Lerne, wie du Dateien und Verzeichnisse verwaltest und suchst.",
-        imageRes = R.drawable.dateisystem_navigation_course
+        imageRes = R.drawable.dateisystem_navigation_course,
+        subcourses = linkedMapOf()
     ),
-    Course(
+    Scenario(
         name = "Textbearbeitung mit vim und nano",
         description = "Werde zum Texteditor-Meister: Lerne die leistungsstarken Texteditoren vim und nano kennen und werde zum Profi in der Textbearbeitung.",
-        imageRes = R.drawable.nano_vs_vim_course
+        imageRes = R.drawable.nano_vs_vim_course,
+        subcourses = linkedMapOf()
     ),
-    Course(
+    Scenario(
         name = "Linux Systemadministration",
         description = "Werde zum Linux-Administrator: Installiere, konfiguriere und verwalte deine eigenen Linux-Systeme. Lerne, wie du Probleme behebst und dein System optimierst.",
-        imageRes = R.drawable.systemadministartion_course
+        imageRes = R.drawable.systemadministartion_course,
+        subcourses = linkedMapOf()
     ),
-    Course(
+    Scenario(
         name = "Netzwerkverwaltung unter Linux",
         description = "Baue deine eigenen Netzwerke: Konfiguriere Netzwerke und richte verschiedene Netzwerkdienste ein. Lerne, wie du mehrere Computer miteinander verbindest und sicher kommunizierst.",
-        imageRes = R.drawable.netzwerkverwaltung_course
+        imageRes = R.drawable.netzwerkverwaltung_course,
+        subcourses = linkedMapOf()
     )
 )
---
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/components/Sublevel.kt
@@ -0,0 +1,13 @@
+package com.example.linux_logic_app.components
+
+/**
+ * Diese Datenklasse namens Subcourse repräsentiert die Attribute eines Sublevels mit dem Titel, der
+ * Beschreibung und dem zugehörigen Index, da es eine Reihenfolge innerhalb der Kurse gibt.
+ * @property name Der Name des Kurses.
+ * @property description Eine kurze Beschreibung des Kurses.
+ */
+data class Sublevel(
+    //val id: Int,
+    val name: String = "Level ",
+    val description: String
+)
