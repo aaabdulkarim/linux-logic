1d5d702618c6f6e70d62de6ad4041bce6f80b87e
bgogoladze <bgogoladze@student.tgm.ac.at>
2025-03-14 13:44:45 +0100

    DA Linux Logic - Frontend App [Commit 137]: Stand 14.03.2025 13:41 es geht an das Schreiben der Implementierung, wo der Code erklärt wird und das Wichtigste zum Vorschein kommt.

--- FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/HomeScreen.kt
+++ FrontendApp/app/src/main/java/com/example/linux_logic_app/screens/HomeScreen.kt
@@ -1,22 +1,103 @@
 package com.example.linux_logic_app.screens
 
-import androidx.compose.runtime.Composable
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.History
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.example.linux_logic_app.components.Terminal
 import com.example.linux_logic_app.components.UserViewModel
+import com.example.linux_logic_app.ui.theme.LiloBlue
 
 @Composable
 fun HomeScreen(userViewModel: UserViewModel) {
-    Box(
+    Column(
         modifier = Modifier
+            .fillMaxSize()
             .background(MaterialTheme.colorScheme.background)
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState())
     ) {
-        Terminal("ws://10.0.107.7:8000/ws", preview = false, userViewModel = userViewModel)
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = LiloBlue,
+                    contentColor = Color.White,
+                ),
+                contentPadding = PaddingValues(32.dp),
+                onClick = {
+                    // Weiterpspielen redirecten
+                },
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        imageVector = Icons.TwoTone.History,
+                        contentDescription = "History Icon for Home",
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f)) // Lässt den Text mittig ausrichten
+
+                    Text(
+                        text = "Nahtlos weiterspielen",
+                        style = MaterialTheme.typography.labelSmall,
+                        modifier = Modifier.align(Alignment.CenterVertically)
+                    )
+
+                    Spacer(modifier = Modifier.weight(1f)) // Platzhalter, um den Button-Inhalt ausgewogen zu halten
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Ihr Fortschritt!",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onBackground,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Terminal("ws://10.0.107.7:8000/ws", preview = false, userViewModel = userViewModel)
+        }
     }
 }
