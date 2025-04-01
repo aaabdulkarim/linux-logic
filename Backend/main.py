d16f777335f5f106b5508edba97e7be090028b96
akarakassoglou <akarakassoglou@student.tgm.ac.at>
2025-04-01 08:52:31 +0200

    von anforderung in terminal geändert

--- Backend/main.py
+++ Backend/main.py
@@ -47,7 +47,7 @@ app = FastAPI()
 origins = ["http://localhost", "http://localhost:8080", "http://localhost:8081"]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
