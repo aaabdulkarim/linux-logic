4c09ab721133354a0daee42989c688ed567ff3dd
amadeus <aabdulkarim@student.tgm.ac.at>
2025-04-01 09:41:08 +0200

    Socket in einer File

--- Backend/main.py
+++ Backend/main.py
@@ -20,6 +20,16 @@ from passlib.context import CryptContext
 import os
 
 
+from fastapi.websockets import WebSocket
+from fastapi import WebSocketDisconnect
+
+from ScenarioTrack import ScenarioTrack
+from DockerManager import DockerManager 
+
+import asyncio
+
+
+
 # docs: https://fastapi.tiangolo.com/tutorial/sql-databases/
 # sqlmodel docs: https://sqlmodel.tiangolo.com/tutorial/where/#where-land
 
@@ -343,3 +353,101 @@ async def logout(response : Response, request: Request,  session : SessionDep):
         response.delete_cookie("session_id", httponly=False, secure=False)
         return {"message": "Logout successful"}
 
+
+
+dm = DockerManager()
+
+
+
+
+@app.websocket("/ws")
+async def websocket(mainsocket: WebSocket):
+    await mainsocket.accept()
+    
+
+    session_id = str(uuid.uuid4())
+    frontend_user_name = await mainsocket.receive_text()    
+    
+    print(frontend_user_name)
+    
+    frontend_container_choice = await mainsocket.receive_text()
+    
+
+    container_name = await dm.add_connection(
+        userSessionId=session_id,
+        userName=frontend_user_name,
+        frontendChoice=frontend_container_choice
+    )
+
+    # TODO: Graceful Closure
+
+    if container_name:
+
+        scm = await dm.get_scm(frontend_user_name, frontend_container_choice)
+
+        # TODO: Nach 1h Inaktivität automatisch schließen 
+        while await dm.get_container_health(container_name) != "healthy":
+            print(await dm.get_container_health(container_name))
+            await asyncio.sleep(2)
+            
+        await mainsocket.send_text("Container Startup successful")
+        container_socket_port = await dm.get_dynamic_port(container_name)
+        print("Das isses: ", container_socket_port)
+
+        # Connection mit dem docker socket mit dem modul websockets
+        container_socket_url = f"ws://127.0.0.1:{container_socket_port}/dockersocket"
+        try:
+            async with websockets.connect(container_socket_url) as container_socket:
+                print("connected to external")
+                while True:
+
+                    # Interaktion mit Frontend Socket
+                    frontend_cmd = await mainsocket.receive_text()
+
+                    try:
+                        if ">clue" == frontend_cmd:
+                            # TODO: SCM Korrekt mit User Connection identifizieren 
+                            # TODO: Update Progress wird nur bei einem Check ausgeführt
+                            scm.update_progress()
+                            clues = "".join(scm.get_clue())
+                            await mainsocket.send_text(clues)
+
+                        if ">check" == frontend_cmd:
+                            await container_socket.send("bash /app/checks_fun.sh")
+                            data = await container_socket.recv()
+                            await mainsocket.send_text(data)
+                            print(data)
+
+                        else:
+                            await container_socket.send(frontend_cmd)
+                            data = await container_socket.recv()
+                            await mainsocket.send_text(data)
+                            print(data)
+
+                
+                    except WebSocketDisconnect:
+                        print("WebSocket client disconnected")
+                        break
+
+        except Exception as e:
+            print(f"Error with external WebSocket: {e}")
+
+        finally:
+            await mainsocket.close()
+            # if container:
+            #     container.stop()
+            #     container.remove()
+            #     print("WebSocket stopped and container removed")
+        
+            await dm.close(container_name)
+
+    else:
+        print("Help")
+
+    
+
+
+@app.get("/")
+async def root():
+    print("Auf root gegangen")
+    return {"message": "Hello World"}
