5962673f50b5a059bb6ba80af3461f3f2d9bcc6d
Benjamin Eder <beder@student.tgm.ac.at>
2025-04-02 17:05:02 +0200

    diverse fixes und erweiterungen

--- FrontendWeb/linux-logic/src/components/HomePage/Kopfzeile.vue
+++ FrontendWeb/linux-logic/src/components/HomePage/Kopfzeile.vue
@@ -36,7 +36,7 @@ export default {
 
 <style scoped>
   .all {
-    height: 102vh;
+    height: 104vh;
     color: white;
     background-color: #569191;
   }
--- FrontendWeb/linux-logic/src/views/AboutView.vue
+++ FrontendWeb/linux-logic/src/views/AboutView.vue
@@ -1,6 +1,4 @@
 <template>
-  <Header />
-  
   <Kopfzeile />
 
   <section id="infos">
