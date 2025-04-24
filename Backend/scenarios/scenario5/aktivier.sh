a3f0a855cbda8bc9ce7aa30f9f9447a55540ebfa
Sergej <kater.fracht@gmail.com>
2025-04-24 18:00:12 +0200

    szenario 5 bash und szenario ueberarbeitet

---
+++ Backend/scenarios/scenario5/aktivier.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Erstelle eine leere Datei namens sicher_ist_sicher.zip
+touch sicher_ist_sicher.zip
+
+# Bestätigung ausgeben
+echo "Die Datei 'sicher_ist_sicher.zip' wurde erfolgreich erstellt."
