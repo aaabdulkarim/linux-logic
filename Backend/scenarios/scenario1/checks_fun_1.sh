cf31125eed0eb6e6ef317a7a810432d2093dfbe3
amadeus <aabdulkarim@student.tgm.ac.at>
2025-04-25 20:54:21 +0200

    OG Check fun 1 with only true/false

--- Backend/scenarios/scenario1/checks_fun_1.sh
+++ Backend/scenarios/scenario1/checks_fun_1.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 set -e
 
 check_file() {
     local file="$1"
-    echo "Überprüfe Datei: '$file'"
     if [ -f "$file" ]; then
         echo "true"
     else
@@ -12,16 +10,4 @@ check_file() {
     fi
 }
 
-check_roter_teppich() {
-    local dir="/home/Veranstaltung"
-    echo "Wechsle in Verzeichnis: '$dir'"
-    cd "$dir" || { echo "Verzeichnis $dir konnte nicht betreten werden"; exit 1; }
-
-    echo "Aktuelles Verzeichnis: $(pwd)"
-    echo "Dateien im aktuellen Verzeichnis:"
-    ls -la
-
-    check_file "roter_teppich.txt"
-}
-
-check_roter_teppich
+check_file "/home/Veranstaltung/roter_teppich.txt"
