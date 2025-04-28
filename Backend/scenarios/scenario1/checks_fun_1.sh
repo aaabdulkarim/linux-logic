065b7f52f4c7c445cfe3c7f6ee69ee37ee50743f
Abdulkarim <aabdulkarim@student.tgm.ac.at>
2025-04-28 22:17:38 +0200

    Verbesserung checks_fun_1.sh

--- Backend/scenarios/scenario1/checks_fun_1.sh
+++ Backend/scenarios/scenario1/checks_fun_1.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+set -e
 
-set -e  # Beende das Skript bei Fehlern
-
-
-# Funktion zur Überprüfung, ob eine Datei existiert
 check_file() {
     local file="$1"
     if [ -f "$file" ]; then
@@ -13,24 +10,4 @@ check_file() {
     fi
 }
 
-# Funktion zur Überprüfung, ob das Verzeichnis existiert
-check_directory() {
-    local dir="$1"
-    if [ -d "$dir" ]; then
-        echo "true"
-    else
-        echo "false"
-    fi
-}
-
-# Einzelne Überprüfung für den roten Teppich
-check_roter_teppich() {
-    # Verzeichnis wechseln
-    cd /home/Veranstaltung || { echo "Verzeichnis /home/Veranstaltung konnte nicht betreten werden"; exit 1; }
-    
-    # Überprüfen, ob die Datei existiert
-    check_file "/home/Veranstaltung/roter_teppich.txt"
-}
-
-# Hauptüberprüfung
-check_roter_teppich
+check_file "/home/Veranstaltung/roter_teppich.txt"
